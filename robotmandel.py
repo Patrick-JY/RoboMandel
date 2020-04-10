96e90d18a800dde61313c77b1f7f662a93530c4e
Patrick <patrickjyoung97@gmail.com>
2020-04-10 14:00:47 +1000

    initial

---
+++ robotmandel.py
@@ -0,0 +1,31 @@
+import discord
+
+client = discord.Client()
+
+
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+    
+
+
+
+
+@client.event
+async def on_member_join(member):
+    await member.send("Welcome to Inverse's discord server! Please Enter your World of Warcraft Character Name")
+
+
+
+    def check(m):
+        return m.author == member
+
+    msg = await client.wait_for('message', check = check)
+
+    await member.edit(nick = msg.content[:12])
+    print(msg.content)
+    await member.send("Thank you")
+   
+
+client.run(SECRET_TOKEN)
